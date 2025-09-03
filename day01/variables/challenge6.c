c8c6053b252947103d18bef9cb43c5212117e588
kinshieslayer <kinkinshie@gmail.com>
2025-09-03 23:37:13 +0100

    Remove unwanted folders

--- day01/variables/challenge6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main() {
-    float a, b;
-    printf("Entrez deux nombres : ");
-    scanf("%f %f", &a, &b);
-    printf("a+b=%.2f\na-b=%.2f\na*b=%.2f\na/b=%.2f\n", a+b, a-b, a*b, a/b);
-    return 0;
-}
